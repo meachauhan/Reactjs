f5d8ef1fd33bcbef1e8200035ee0a038ac99a9b5
Akash Chauhan <meachauhan@gmail.com>
2024-08-09 19:27:22 +0530

    Creating React Element with Java Script

--- 01Basics/src/main.jsx
+++ 01Basics/src/main.jsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const evaluatedExpression="this is a String"
+
+// Creating element without using HTML
+const reactElement=React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'Click here to visit google.com ',
+  evaluatedExpression
+
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  reactElement
+
 )
