20558999ecb3b8b4698d7bb1fdd0885bff81607c
Akash Chauhan <meachauhan@gmail.com>
2024-08-13 11:01:09 +0530

    tailwind+props

---
+++ 03tailwindwithprops/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Card from './components/Card';
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-600 p-3 text-black rounded-xl mb-6'> Tailwind test </h1>
+      <Card product="Macbook" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. "/>
+      <Card product="Laptop" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. "/>
+    </>
+  )
+}
+  
+
+export default App
