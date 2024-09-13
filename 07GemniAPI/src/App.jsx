539fb67528489710fa16a7ca6ab6923fa39061cc
Akash Chauhan <meachauhan@gmail.com>
2024-09-13 14:09:32 +0530

    GeminiAIProject Setup + FrontEnd v0.1

---
+++ 07GemniAPI/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+      <div className='bg-gray-900 w-full h-screen  fixed flex flex-col items-center p-10'>
+       
+        <h1 className='text-blue-900 text-7xl font-sans  '>Gemini API </h1>
+        <input type="text" name="query" id="query" 
+        className="m-10 w-3/6 py-5 px-4 outline-none text-blue-200  bg-gray-700 rounded-3xl overflow-hidden"
+        placeholder="Enter your query" />
+      
+        
+      
+        
+      </div>  
+    
+  )
+}
+
+export default App
